0e05341c8e3a7e0324db7f275eff504e28caff1b
Ravisingh13 <41977362+Ravisingh13@users.noreply.github.com>
2022-02-17 00:39:41 +0530

    Add files via upload

---
+++ Day2/JS.js
@@ -0,0 +1,26 @@
+function changecolortext1() {
+var x = document.getElementById("div1");
+
+x.style.color = 'red';
+
+}
+
+function colorblack1() {
+var x = document.getElementById("div1");
+
+x.style.color = 'black';
+
+}
+
+function changecolortext2() {
+    // var x = document.getElementById("div1");
+    var y = document.getElementById("div2");
+    
+    y.style.color = 'red';
+}
+function colorblack2() {
+    // var x = document.getElementById("div1");
+    var y = document.getElementById("div2");
+    
+    y.style.color = 'black';
+}
